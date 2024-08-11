dc42c3a2d7a8430d90a1fd4aef80b6cccab789c1
gaetanodigrazia <gaetano.digrazia@bitrock.it>
2024-08-11 23:05:04 +0200

    Initial commit

---
+++ src/main/java/com/digrazia/sql_demo/SqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.digrazia.sql_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digrazia/sql_demo/SqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digrazia.sql_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
